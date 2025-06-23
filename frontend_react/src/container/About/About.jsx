9e9a6c25c4627d20c072001386aadfcd811e45e4
taskin-sky <taskinmubassir@gmail.com>
2025-06-23 14:25:09 +0600

    about

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
+// import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
+import { images } from '../../constants';
+// import { urlFor, client } from '../../client';
 
-export const About = () => {
+const abouts = [
+  { title: 'Web Development', description: 'I am a Web developer.', imgUrl: images.about01 },
+
+  { title: 'Frontend Development', description: 'I am a Web developer.', imgUrl: images.about02 },
+  
+  { title: 'Backend Development', description: 'I am a Web developer.', imgUrl: images.about03 },
+  
+  { title: 'MERN Development', description: 'I am a Web developer.', imgUrl: images.about04 }
+];
+
+const About = () => {
+  // const [abouts, setAbouts] = useState([]);
+
+  // useEffect(() => {
+  //   const query = '*[_type == "abouts"]';
+
+  //   client.fetch(query).then((data) => {
+  //     setAbouts(data);
+  //   });
+  // }, []);
+
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            {/* add urlFor */}
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default About
+
+// export default AppWrap(
+//   MotionWrap(About, 'app__about'),
+//   'about',
+//   'app__whitebg',
+// );
