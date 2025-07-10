e941a2f427bfb42e2b81b8d7916f1c964c35c136
Taskin Mohammad Mubassir <taskinmubassir@gmail.com>
2025-07-10 13:44:47 +0600

    backend connection and upgrade about

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -4,28 +4,28 @@ import { motion } from 'framer-motion';
 // import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
 import { images } from '../../constants';
-// import { urlFor, client } from '../../client';
+import { urlFor, client } from '../../client';
 
-const abouts = [
-  { title: 'Web Development', description: 'I am a Web developer.', imgUrl: images.about01 },
+// const abouts = [
+//   { title: 'Web Development', description: 'I am a Web developer.', imgUrl: images.about01 },
 
-  { title: 'Frontend Development', description: 'I am a Web developer.', imgUrl: images.about02 },
+//   { title: 'Frontend Development', description: 'I am a Web developer.', imgUrl: images.about02 },
   
-  { title: 'Backend Development', description: 'I am a Web developer.', imgUrl: images.about03 },
+//   { title: 'Backend Development', description: 'I am a Web developer.', imgUrl: images.about03 },
   
-  { title: 'MERN Development', description: 'I am a Web developer.', imgUrl: images.about04 }
-];
+//   { title: 'MERN Development', description: 'I am a Web developer.', imgUrl: images.about04 }
+// ];
 
 const About = () => {
-  // const [abouts, setAbouts] = useState([]);
+  const [abouts, setAbouts] = useState([]);
 
-  // useEffect(() => {
-  //   const query = '*[_type == "abouts"]';
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
 
-  //   client.fetch(query).then((data) => {
-  //     setAbouts(data);
-  //   });
-  // }, []);
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
@@ -40,8 +40,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
-            {/* add urlFor */}
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
