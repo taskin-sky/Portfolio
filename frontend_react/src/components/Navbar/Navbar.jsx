fae4458a09965e9c1deaa8c80ada66072ad84b4d
Taskin Mohammad Mubassir <taskinmubassir@gmail.com>
2025-07-15 04:59:45 +0600

    update full

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
             <img src={images.logo} alt="logo" />
         </div>
         <ul className="app__navbar-links">
-            {['home', 'about', 'contact', 'work', 'skills', 'contact'].map((item) => (
+            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
                 <li className="app__flex p-text" key={`link-${item}`}>
                     <div/>
                     <a href={`#${item}`}>{item}</a>
